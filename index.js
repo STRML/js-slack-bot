9a54466b33e2c0962c43abcfb395f5e702129502
Samuel Reed <samuel.trace.reed@gmail.com>
2016-06-06 16:33:34 -0500

    Add babel transpilation like the repl

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const Botkit = require('botkit');
 const vm = require('vm');
 const fs = require('fs');
 const path = require('path');
+const babel = require('babel-core');
+
+const BABEL_PRESETS = ['es2015', 'node6', 'stage-0', 'stage-1', 'stage-2', 'stage-3'];
 
 const token = fs.readFileSync(path.join(__dirname, '.token')).toString('utf8').trim();
 
 var controller = Botkit.slackbot({
-  debug: true,
+  debug: false,
 });
 
 controller.spawn({
@@ -17,15 +20,36 @@ controller.spawn({
   if (err) throw new Error(err);
 });
 
-controller.hears(['[\s\S]*'],['direct_message','direct_mention','mention', 'ambient'],function(bot,message) {
+const codeRegex = /(^`+|`+$)/g;
+const babelRegex = /^babel: ?(.*)/i;
+controller.hears(['[\s\S]*'],['direct_message','direct_mention','mention'], function(bot, message) {
   console.log("heard it, ", message);
+  let {text} = message;
+  text = text.replace(codeRegex, '');
+
+  // Ping
+  if (text === 'ping') return bot.reply('pong');
+
+  try {
+    // Babel transpile
+    const match = text.match(babelRegex);
+    if (match && match.length) {
+      let code = match[1];
+      code = code.replace(codeRegex, '');
+      return bot.reply(message, '```' + babel.transform(code, {presets: BABEL_PRESETS}).code + '```');
+    }
+
+    // Regular eval
+    const result = vm.runInContext(text, createSandbox());
+    bot.reply(message, `\`${result.toString()}\``);
+  } catch (e) {
+    bot.reply(message, `\`${e.toString()}\``);
+  }
+});
+
+function createSandbox() {
   const sandbox = {};
   Object.freeze(sandbox);
   vm.createContext(sandbox);
-  try {
-    const result = vm.runInContext(message.text, sandbox);
-    bot.reply(message, result.toString());
-  } catch (e) {
-    bot.reply(message, e.toString());
-  }
-});
+  return sandbox;
+}
